2ba8ad00a376467718791d92eae10a9b8fee5d61
permCoding <ttxiom@gmail.com>
2026-01-21 19:04:47 +0500

    up

---
+++ tasks/26/26_23383.py
@@ -0,0 +1,25 @@
+# есть полные дубликаты
+# [31742, 15]
+# [31742, 15]
+
+w = open('./26_23383.txt').readlines()[1:]
+t = [list(map(int, e.split())) for e in w]
+t.sort(key=lambda x: (x[1], x[0]))
+
+m, k, n = 1, 1, -1
+for i in range(1, len(t)):
+    u1 = t[i][1] == t[i-1][1]
+    u2 = t[i][0] - 1 == t[i-1][0]
+    u3 = t[i][0] == t[i-1][0]
+    if u1:
+        if u3: continue
+        if u2:
+            k += 1
+            if k > m:
+                m = k
+                n = t[i][1]
+        else:
+            k = 1
+    else:
+        k = 1
+print(m, n)
