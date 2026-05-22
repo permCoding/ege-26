1e188c2c0d2cbd9fd05b04e34e4ea860152381b3
permCoding <ttxiom@gmail.com>
2026-05-22 21:04:35 +0500

    p

--- tasks/examples/ЕКГР-18-04-2026/26-28945-.py
+++ tasks/examples/ЕКГР-18-04-2026/26-28945-.py
@@ -1,5 +1,4 @@
 f = open("26_28945.txt")
-
 n = int(f.readline())
 
 t = []
@@ -8,15 +7,15 @@ for _ in range(n):
     t.append((st, st+ln))
     if ln == 184: print((st, st+ln))
 
-t.sort(key=lambda x: (x[1], -x[0]))
+t.sort(key=lambda x: x[1])
 
 res = [t[0]]
-j = 0
 for i in range(1, n):
     if t[i][0] >= res[-1][1]:
         res += [t[i]]
-        j = i
-print(len(res), j, n)  # 77
+print(len(res), 10_000-res[-1][1])  # 77
 
 print(res[-2], res[-1])
-for i in range(j-100, n): print(t[i], t[i][1]-t[i][0])
+for e in t[-20:]:
+    if e[0] >= res[-2][1]:
+        print(e, 10_000-e[1])  # 184
