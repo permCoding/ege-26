a53b9600f366bac5b6d63b90d0636f5d79b8ba70
permCoding <ttxiom@gmail.com>
2026-05-28 22:18:40 +0500

    p

--- tasks/24/max_24_29354_v2.py
+++ tasks/24/max_24_29354_v2.py
@@ -1,13 +1,22 @@
 s = open("24-29354.txt").readline().strip()
-t = [i for i in range(len(s)-1) if s[i]+s[i+1] == 'BC']
-t = [-2] + t + [len(s)]
 
-d = 191
-r = [t[j+d]-(t[j]+2) for j in range(len(t)-d)]
-print(max(r))
+t = [-2]+[i for i in range(len(s)-1) if s[i:i+2] == 'BC']+[len(s)]
 
+d = 190
+max_len = 0
+for j in range(len(t) - d - 1):
+    ln = t[j+d+1] - t[j] - 2
+    max_len = max(max_len, ln)
+print(max_len)  # 2285 | но 2287 - правильный ответ
 
-
+# t = [-2]+[i for i in range(len(s)-1) if s[i:i+2] == 'BC']+[len(s)]
+#
+# d = 190
+# r = [t[j+d+1]-(t[j]+2) for j in range(len(t)-d-1)]
+# print(max(r)) # 2285 | но 2287 - правильный ответ
+#
+# s = 'BC**BC***BC*BC'  # len=4
+# s = '12BC345BC6BCBC'  # len=4
 
 # mx, cntBC, l = 0, 0, 0
 # for r in range(1, len(s)):
